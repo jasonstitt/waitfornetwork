db08f2ccfc3fe07217eaafea432f4078d4337012
Jason Stitt <js@jasonstitt.com>
2024-04-02 20:31:51 -0500

    echo rust target in build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 declare -A targets
 targets=(['linux/arm64']='aarch64' ['linux/amd64']='x86_64')
-echo $TARGETPLATFORM
 export RUSTTARGET=${targets[${TARGETPLATFORM}]}-unknown-linux-musl
+echo $TARGETPLATFORM $RUSTTARGET
 rustup target add $RUSTTARGET
 cargo install --target $RUSTTARGET --path .
